6971b18b5d53d00ff6588f20b4e76227328d1d23
jgietz <jgietz@console.com.au>
2017-04-27 11:12:26 +1000

    Added pages to reducer/container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,10 +1,10 @@
 export default function() {
   return [
-    { title: 'The Lord Of The Rings' },
-    { title: 'Wizards First Rule' },
-    { title: 'Dune' },
-    { title: 'The Painted Man' },
-    { title: 'Storm Front' },
-    { title: 'Tomorrow When The War Began' }
+    { title: 'The Lord Of The Rings', pages: 1003 },
+    { title: 'Wizards First Rule', pages: 200 },
+    { title: 'Dune', pages: 500 },
+    { title: 'The Painted Man', pages: 200 },
+    { title: 'Storm Front', pages: 300 },
+    { title: 'Tomorrow When The War Began', pages:150 }
   ]
 }
