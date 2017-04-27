6e77b4be40393f9f2db398f58c8bdd699b136e65
jgietz <jgietz@console.com.au>
2017-04-27 11:02:17 +1000

    Created book container

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
 export function selectBook(book) {
-  console.log('A book has been selected:', book.title);
+  // selectBook is an ActionCreator, it needs to return and action,
+  // an object with a type property.
+  return {
+    // Very important to have case consistant so we stick with upercase and snake_case
+    // The action itself needs to be an object with a type property
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
